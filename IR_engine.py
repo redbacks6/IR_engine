c82f8650bc5e9e8f6c481fbf24727dc229e1a48a
Luke Jones <lukealexanderjones@gmail.com>
2015-03-24 23:02:12 +1100

    updated object to include saving and loading of index
    Not quite working on the load yet.
    Queries updated to process all queries into a list of tuples.

--- IR_engine.py
+++ IR_engine.py
@@ -12,34 +12,26 @@ import nltk, re, os, math, numpy as np
 from glob import glob
 from pprint import pprint as pp
 from nltk.corpus import stopwords
+try:
+    import cPickle as pickle
+except:
+    import pickle
 
 corpus = "/Users/lukejones/Desktop/corpus/this_old_man/doc*.txt"
 corpus1 = "/Users/lukejones/Desktop/University/web_search_and_text_analysis/proj1data/blogs/*.txt"
+output_index = '/Users/lukejones/Desktop/University/web_search_and_text_analysis/proj1index/index1.txt'
+output_documents = '/Users/lukejones/Desktop/University/web_search_and_text_analysis/proj1index/documents1.txt'
+
 porter = nltk.PorterStemmer()
 stopwords = nltk.corpus.stopwords.words('english')
 
 def main():
-	#clear the console
-	os.system('cls' if os.name == 'nt' else 'clear')
-
-
-	print ('Welcome to the Information Retrieval Engine!')
-	print 'The IREngine takes a corpus in the form of directory/documents/*.txt ' \
-		  'and lets you query it.'
-	keyboard = raw_input("\nEnter the location of your corpus: ")
-
-	index = IREngine(keyboard)
-
-	print ('\nInformation Retrieval Engine is ready!')
-	print ('Enter your query or type quit() to quit.')
-
-	while True:
-		keyboard = raw_input("\nEnter your query here: ")
-		if keyboard.lower() == 'quit()':
-			break
-		else:
-			print('\nTop Results for query: %s' %(keyboard))
-			pp(index.query(keyboard)[:10])
+	
+	index = IREngine()
+	# index.build_index(corpus)
+	# index.write_index_to_file(output_index, output_documents)
+	index.load_index(output_index, output_documents)
+	print pp(index.query('old man')[:10])
 
 
 """
@@ -50,36 +42,21 @@ It also has a query function
 """
 class IREngine:
 
-	def __init__(self, corpus):
-
+	def __init__(self):
 		#corpus = link to a corpus
-		self.corpus = corpus
-		self.corpus_size = len(glob(corpus))
+		self.corpus = ''
 		# documents = {DocID:|w,d| }
 		self.documents = {}
 		#inverted_index = {Term: IDF, [(DocID, No. Postings)]}
 		self.index = {}
 
-		#build inverted index
-		count = 1
+	def build_index(self, corpus):
+		self.corpus = corpus
+		process_corpus(self.corpus, self.index, self.documents)
 
-		print '\nProcessing Documents. Total Corpus size is %s documents' %(self.corpus_size)
-		for document in glob(corpus):
-			raw = open(document).read()
-			doc = [os.path.basename(document),process_text_doc(raw)]
-			self.documents[doc[0]] = {}
-			self.documents[doc[0]] = len(doc[1])
-			self.index = add_doc_to_index(doc, self.index)
-			
-			if (count % 500) == 0:
-				print "Processed %s of %s documents" %(count, self.corpus_size)
-			
-			# if count > 1000:
-			# 	break
-			count += 1
-
-		#finalise inverted index
-		updateIDF(self.index,self.documents)
+	def load_index(self, index, documents):
+		self.index = pickle.loads(index)
+		self.documents = pickle.loads(documents)	
 
 
 	"""
@@ -91,12 +68,11 @@ class IREngine:
 	Output: Ranked results
 	Return: [document, score]
 	"""
-
 	def query(self, terms):
-
 		query = process_text_doc(terms)
-
 		results = {}
+		finalresults = []
+
 		for key in self.documents:
 			results[key] = 0
 
@@ -104,8 +80,6 @@ class IREngine:
 			if term in self.index:
 				for document in self.index[term][1]:
 					results[document] = results[document] + self.index[term][1][document] * self.index[term][0]
-
-		finalresults = []
 
 		for document in results:
 			if results[document] != 0 and self.documents[document] != 0:
@@ -116,6 +90,48 @@ class IREngine:
 		else:
 			return ['There seems to be no results for your query']
 
+
+	"""
+	Writes the index to a file for quick retrieval later
+	Input: URL of file
+	"""
+	def write_index_to_file(self, index_url, documents_url):
+		index_string = pickle.dumps(self.index)
+		documents_string = pickle.dumps(self.documents)
+		index_doc = open(index_url,"w")
+		docs_doc = open(documents_url,"w")
+		index_doc.write(index_string)
+		docs_doc.write(documents_string)
+		index_doc.close()
+		docs_doc.close()
+
+"""
+Processes corpus and updates an index and documents dictionary
+"""
+def process_corpus(corpus, index, documents):
+	#build inverted index
+	count = 1
+	corpus_size = len(glob(corpus))
+
+	print '\nProcessing Documents. Total Corpus size is %s documents' %(corpus_size)
+	for document in glob(corpus):
+		raw = open(document).read()
+		doc = [os.path.basename(document),process_text_doc(raw)]
+		documents[doc[0]] = {}
+		documents[doc[0]] = len(doc[1])
+		index = add_doc_to_index(doc, index)
+		
+		if (count % 500) == 0:
+			print "Processed %s of %s documents" %(count, self.corpus_size)
+		
+		# if count > 1000:
+		# 	break
+		count += 1
+
+	#finalise inverted index
+	updateIDF(index, documents)		
+
+	return index, documents		
 
 def updateIDF(index, documents):
 	N = float(len(documents))
@@ -154,7 +170,6 @@ Input: document as a string
 Output: Tokenised document
 Return: [filename, [tokens]]
 """
-
 def process_text_doc(document):
 	#remove formatting and punctuation and non alpha numeric characters
 	#fold to lower case
@@ -166,7 +181,32 @@ def process_text_doc(document):
 
 	return final_list
 
+"""
+This function allows the user to run the IREngine as a 
+standalone console based program.
+"""
+def run_as_program():
+	#clear the console
+	os.system('cls' if os.name == 'nt' else 'clear')
 
+
+	print ('Welcome to the Information Retrieval Engine!')
+	print 'The IREngine takes a corpus in the form of directory/documents/*.txt ' \
+		  'and lets you query it.'
+	keyboard = raw_input("\nEnter the location of your corpus: ")
+
+	index = IREngine(keyboard)
+
+	print ('\nInformation Retrieval Engine is ready!')
+	print ('Enter your query or type quit() to quit.')
+
+	while True:
+		keyboard = raw_input("\nEnter your query here: ")
+		if keyboard.lower() == 'quit()':
+			break
+		else:
+			print('\nTop Results for query: %s' %(keyboard))
+			pp(index.query(keyboard)[:10])
 
 # Run the Main Method
 if __name__ == '__main__':
--- queries.py
+++ queries.py
@@ -6,24 +6,25 @@ Student ID: 654645
 Date: 24 March 2015
 """
 
-import re
+import re 
+from pprint import pprint as pp
 
 url = '/Users/lukejones/Desktop/University/web_search_and_text_analysis/proj1data/06.topics.851-900.txt'
 
-query_text = open(url).read()
 
-number = re.compile(r"(?<=Number:\s).*")
-title = re.compile(r"(?<=<title>\s).*")
 
-#starting number of the topic
-topic = 851
+def extract_queries():
+	query_text = open(url).read()
+	title = re.compile(r"(?<=<title>\s).*")
 
-query_array = []
+	#starting number of the topic
+	topic = 851
+	query_array = []
 
-print query_text
+	output = title.findall(query_text)
 
-if title.search(query_text):
-	query_array.append((topic,title.search(query_text)))
-	topic += 1
+	for query in output:	
+		query_array.append((topic,query))
+		topic += 1
 
-print query_array	
+	pp(query_array)
